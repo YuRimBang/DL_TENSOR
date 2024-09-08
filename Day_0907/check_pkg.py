45dc5118f801533c9ec8f05a0cb104114c67208e
YuRimBang <qkddbfla1@naver.com>
2024-09-08 10:21:27 +0900

    init

---
+++ Day_0907/check_pkg.py
@@ -0,0 +1,16 @@
+#설치 패키지 사용 가능 여부 체크
+import pandas as pd
+import numpy as np
+import sklearn
+import matplotlib
+import tensorflow as tf
+from tensorflow import keras
+import seaborn
+
+print(f'pandas V.{pd.__version__}')
+print(f'numpy V.{np.__version__}')
+print(f'tensorflow V.{tf.__version__}')
+print(f'sklearn V.{sklearn.__version__}')
+print(f'seaborn V.{seaborn.__version__}')
+print(f'matplotlib V.{matplotlib.__version__}')
+print(f'keras V.{keras.__version__}')
